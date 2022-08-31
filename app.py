24075e44f435d59ef811145bf043f18dccc6fad6
JouniKiiskila <77729162+JouniKiiskila@users.noreply.github.com>
2022-08-31 10:57:26 +0300

    Update to Wiki

---
+++ app.py
@@ -0,0 +1,15 @@
+# SOVELLUKSEN PÄÄOHJELMA
+
+# Kirjastojen ja modulien lataus
+import datetime
+
+# Luokka-määritykset
+
+class Person():
+    """Class for storing person information"""
+    def __init__(self, firstName, lastName):
+        self.firstName = firstName
+
+        
+
+    
